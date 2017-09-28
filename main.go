8ab0bf535bec65fcb80f5af7a64236201e531d7c
Filip Hesslund <filip@hesslund.org>
2017-09-28 15:33:39 +0200

    Don't re-register device each run. Just check for messages.

--- main.go
+++ main.go
@@ -5,14 +5,20 @@ import (
 )
 
 func main() {
-	var c Credentials
-	c.GetCredentials()
+	//var c Credentials
+	//c.GetCredentials()
 
-	var ar AuthResp
-	ar.Auth(c.Username, c.Password)
-	fmt.Println(ar)
+	//var ar AuthResp
+	//ar.Auth(c.Username, c.Password)
+	//fmt.Println(ar)
 
-	var d DeviceResp
-	d.Register("test-device")
-	fmt.Println(d)
+	//var d DeviceResp
+	//d.Register("test-device")
+	//fmt.Println(d)
+
+	var mr MessageResp
+	mr.LookUpMessages()
+	messages := mr.GetMessages()
+	fmt.Println(messages)
+	//messages[len(messages)-1].UpdateToLatest()
 }
