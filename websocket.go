6bc3e1dda71eeb6ef5590f7d27b8262f3dcdccb9
Filip Hesslund <filip@hesslund.org>
2017-12-12 19:24:34 +0100

    Continuously fetch messages.
    Continuously read and print messages. Added som TODOs.

--- websocket.go
+++ websocket.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/net/websocket"
 	"log"
+
+	"golang.org/x/net/websocket"
 )
 
 const (
@@ -24,23 +25,34 @@ func Connect() {
 	}
 
 	for {
-		msg := make([]byte, 512)
+		msg := make([]byte, 1)
 		_, err := ws.Read(msg)
 		if err != nil {
 			log.Fatal(err)
 		}
-		m := string(msg)
-		fmt.Println(m)
-		switch m {
-		case strings.HasPrefix(m, "!"):
-			Sync()
-		case strings.HasPrefix(m, "#"):
-			continue
-		case strings.HasPrefix(m, "R"):
-			ReConnect()
-		default:
+		// TODO: Do some logging instead
+		code := msg[0]
+		switch {
+		case code == 33: // New message received ("!")
+			var mr MessageResponse
+			mr.LookUpMessages()
+			messages := mr.GetMessages()
+			message := messages[len(messages)-1]
+			fmt.Println(message)
+			message.SetAsLatest()
+		case code == 35: // Keep-alive message ("#")
+			fmt.Println("Keep-alive message received")
+		case code == 69: // Error message ("E")
+			fmt.Println("Something horrible happened!")
+			fmt.Println("Try to login again.")
 			return
+		case code == 82: // Reconnect message ("R")
+			ws.Close()
+			// TODO: Close connection and reconnect gracefully
+		default:
+			fmt.Println("Didn't match anything")
 		}
+		// TODO: Handle emergency messages as per https://pushover.net/api/client#p2
 	}
 
 }
