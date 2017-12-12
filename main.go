07da6ac1241b79816f4cab08d0027f6492a86650
Filip Hesslund <filip@hesslund.org>
2017-12-12 19:23:29 +0100

    Testing

--- main.go
+++ main.go
@@ -4,22 +4,27 @@ package main
 //	"fmt"
 //)
 
+// TODO: Refactor the shit out of this
+// Fetch more messages on startup before opening websocket connection
+// and continuously getting messages.
+// TODO: Only register device when necessary.
+// TODO: Prompt for credentials and save them in some proper place.
 func main() {
-	//var c Credentials
-	//c.GetCredentials()
+	// var c Credentials
+	// c.GetCredentials()
 
-	//var ar AuthResp
-	//ar.Auth(c.Username, c.Password)
-	//fmt.Println(ar)
+	// var ar AuthResp
+	// ar.Auth(c.Username, c.Password)
+	// fmt.Println(ar)
 
-	//var d DeviceResp
-	//d.Register("test-device")
-	//fmt.Println(d)
+	// var d DeviceResp
+	// d.Register("test-device-laptop")
+	// fmt.Println(d)
 
-	//var mr MessageResp
-	//mr.LookUpMessages()
-	//messages := mr.GetMessages()
-	//fmt.Println(messages)
+	// var mr MessageResp
+	// mr.LookUpMessages()
+	// messages := mr.GetMessages()
+	// fmt.Println(messages)
 	//length := len(messages)
 	//lastMessage := messages[length-1]
 	//fmt.Println(lastMessage)
